0325b31d2506caef9d95c9b17a8c3414f0c8bf82
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-06-10 21:50:45 -0400

    added dependencies and some documentation, starting to setup for Twitter functionality

--- liri.js
+++ liri.js
@@ -1,10 +1,21 @@
-// node.js “Liri” BOT — version 0.1
-/* ARGUMENTS:
-* my-tweets
-* spotify-this-song
-* movie-this
-* do-what-it-says
+// node.js “Liri” BOT — version 0.2
+/** INSTRUCTIONS:
+* node liri.js TASK REQUEST
+* TASKS:
+  * @param my-tweets
+  * @param spotify-this-song
+  * @param movie-this
+  * @param do-what-it-says
 */
+/* DEPENDENCIES: */
+var fs = require('fs'); //fs node library allows file-system functionality
+var request = require('request'); //request node library
+var LiriTwitterBOT = require("./keys.js"); //store data from keys.js
+var twitter = require('twitter'); //twitter node library
 
-// grab data from keys.js and store it into a variable
-var LiriBOT = require("./keys.js");
+//TwitterAPI calls (using keys):
+var tweets = new twitter(LiriTwitterBOT.LiriTwitterBOT);
+var tSearch = { //Twitter search params
+  screen_name: 'ForbesGriffith',
+  count: 20
+}
