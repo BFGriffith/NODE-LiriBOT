68cee95c11abd3a7842cc8df01c91cc9581b5fb7
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-06-10 22:07:34 -0400

    added process.argv captures for tasks and inquiry requests, plus a writeLog function using the fs library

--- liri.js
+++ liri.js
@@ -13,6 +13,19 @@ var request = require('request'); //request node library
 var LiriTwitterBOT = require("./keys.js"); //store data from keys.js
 var twitter = require('twitter'); //twitter node library
 
+var task = process.argv[2]; //captures user TASK selection
+var inquiry = process.argv.slice(3).join('+'); //captures user inputs after task selection for use as parameters for API requests
+
+/* WRITE to log.txt */
+function writeLog(textLog) {
+  fs.appendFile('log.txt', textLog, function(err) {
+    if (err) {
+      return console.log(err);
+    };
+    console.log('log.txt has been updated!'); //informs user of log update
+  });
+}; //END-writeLog
+
 //TwitterAPI calls (using keys):
 var tweets = new twitter(LiriTwitterBOT.LiriTwitterBOT);
 var tSearch = { //Twitter search params
