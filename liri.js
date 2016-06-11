30d3a596693530c76b420333e3bddf0e9ea89733
BFGriffith <benjaminforbesgriffith@gmail.com>
2016-06-10 23:08:22 -0400

    having problems with Twitter keys/OAuth but I think the function should be working

--- liri.js
+++ liri.js
@@ -16,6 +16,15 @@ var twitter = require('twitter'); //twitter node library
 var task = process.argv[2]; //captures user TASK selection
 var inquiry = process.argv.slice(3).join('+'); //captures user inputs after task selection for use as parameters for API requests
 
+//TwitterAPI calls (using keys.js):
+var tweets = new twitter(LiriTwitterBOT.LiriTwitterBOT);
+var tSearch = { //Twitter search params
+  screen_name: 'ForbesGriffith',
+  count: 20,
+}
+
+//initialize string variable to be written to log.txt
+var logObject = "";
 /* WRITE to log.txt */
 function writeLog(textLog) {
   fs.appendFile('log.txt', textLog, function(err) {
@@ -41,9 +50,19 @@ switch (task) {
     console.log('The prompt you typed for the requested task was not recognized; please double-check your spelling and try again.');
 }; //END-SWITCH
 
-//TwitterAPI calls (using keys):
-var tweets = new twitter(LiriTwitterBOT.LiriTwitterBOT);
-var tSearch = { //Twitter search params
-  screen_name: 'ForbesGriffith',
-  count: 20
-}
+/* TWITTER function: last 20 tweets from user based on set parameters */
+function fetchTweets() {
+  tweets.get('statuses/user_timeline', tSearch, function(err, response) {
+    if (err) {
+      console.log(err);
+    };
+    console.log('These are my last ' + response.length + ' tweets.')
+    for (var i = 0; i < response.length; i++) {
+      console.log('#' + (i + 1) + " = " + response[i].text); //log tweet message
+      console.log('posted on: ' + response[i].created_at); //log creation date
+      logObject += ', ' + '#' + (i + 1) + " = " + response[i].text + response[i].created_at; //concatenation to log.txt
+    };
+    logObject = task + "" + logObject + "\n"; //task to beginning
+    writeLog(logObject); //write to log.txt
+  });
+}; //END-fetchTweets
